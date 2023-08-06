dce1e1121daa0bcc41462427c646d301feddfe69
Adeliya-Valiulina <adeliavaliulina@gmail.com>
2023-08-07 02:16:54 +0300

    create: added slider reviews

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-var mySwiper = new Swiper('.swiper', {
+var productSwiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+var reviewsSwiper = new Swiper('.reviews-slider', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
